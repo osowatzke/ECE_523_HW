3ead59cdec869d4babef2500118660bcbdf27d10
Owen <osowatzke@gmail.com>
2024-03-10 18:34:13 -0700

    Cleaned up matlab code

--- 3/Homework3_Sowatzke.m
+++ 3/Homework3_Sowatzke.m
@@ -1,10 +1,12 @@
 %% Problem 1
+% Data vectors
 X1 = [ 1,  1;
       -1, -1]';
 
 X2 = [ 1, -1;
       -1,  1]';
 
+% Function to increase dimension of data
 phi = @(X)([ones(1,size(X,2)); ...
             sqrt(2)*X(1,:); ...
             sqrt(2)*X(2,:); ...
@@ -12,85 +14,68 @@ phi = @(X)([ones(1,size(X,2)); ...
             X(1,:).^2; ...
             X(2,:).^2]);
 
+% Apply function to samples of eah data class
 X1 = phi(X1);
 X2 = phi(X2);
+
+% Solve for the linear discriminat function
 [w,b] = svm(X1,X2);
+
+% Display solution for problem 1
 fprintf('Problem 1:\n');
 fprintf('w\n')
 disp(w);
 fprintf('b = %g\n', b);
 fprintf('\n');
 
-% Problem 2
+%% Problem 2
+% Data vectors
 X1 = [1 1;
       2 2;
       2 0].';
 X2 = [0 0;
       1 0;
       0 1].';
-f = figure(1);
+
+% Create figure
+figure(1);
 clf;
-% f.Position(3) = min([f.Position(3), f.Position(4)]);
-% f.Position(4) = f.Position(3);
-scatter(X1(1,:),X1(2,:),50,'X','LineWidth',1.5);
 hold on;
+
+% Plot points for each class
+scatter(X1(1,:),X1(2,:),50,'X','LineWidth',1.5);
 scatter(X2(1,:),X2(2,:),50,'O','LineWidth',1.5);
+
+% Set plot bounds
 xlim([-1 3]);
 ylim([-1 3]);
+
+% Plot the weight vector and margin
 plot([-1, 2.5],[2.5 -1],'k','LineWidth',1.5);
 plot([-1 3],[3 -1],'k--');
 plot([-1 2],[2 -1],'k--');
 axis square;
+
+% Label margin
 doublearrow([0, 0.5],[1 1.5]);
 text(0,1.25,'M');
-box on;
+
+% Add legend
 legend('Class \omega_1','Class \omega_2','Weight vector');
+box on;
 
-X1 = [1, 1;
-      2, 2;
-      2, 0]';
-X2 = [0, 0;
-      1, 0;
-      0, 1]';
-X = [X1 -X2];
-H = X'*X;
-f = -ones(size(X,2),1);
-A = [1 1 1 -1 -1 -1];
-b = 0;
+% Solve for equivalent linear discrimant function
+[w,b] = svm(X1,X2);
 
-[a,FVAL] = quadprog(H,f,[],[],A,b,zeros(6,1),[],[],options);
-w = X*a;
-[~,I] = max(a);
-if (I <= 3)
-    b = 1 - w'*X(:,I);
-else
-    b = -1 - w'*X(:,I);
-end
+% Display solution for problem 2
+fprintf('Problem 1:\n');
+fprintf('w\n')
+disp(w);
+fprintf('b = %g\n', b);
+fprintf('\n');
 
-function doublearrow(x,y)
-    [x,y] = toNormalizedUnits(x,y);
-    annotation('doublearrow',x,y)
-end
-
-function [x,y] = toNormalizedUnits(x,y)
-    ax = gca;
-    xLim = xlim;
-    yLim = ylim;
-    xRange = xLim(2) - xLim(1);
-    yRange = yLim(2) - yLim(1);
-    xSf = ax.Position(3)*ax.Parent.Position(3)/xRange;
-    ySf = ax.Position(4)*ax.Parent.Position(4)/yRange;
-    sf = min([xSf,ySf]);
-    xSf = sf/ax.Parent.Position(3);
-    ySf = sf/ax.Parent.Position(4);
-    xCenter = ax.Position(1) + ax.Position(3)/2;
-    yCenter = ax.Position(2) + ax.Position(4)/2;
-    xStart = xCenter - xRange/2*xSf;
-    yStart = yCenter - yRange/2*ySf;
-    x = (x - xLim(1))*xSf + xStart;
-    y = (y - yLim(1))*ySf + yStart;
-end
-
+% Function solves for a linear discriminant function using a support
+% vector machine
 function [w, b] = svm(X1, X2)
     X_hat = [X1, -X2];
     H = X_hat'*X_hat;
@@ -109,3 +94,48 @@ function [w, b] = svm(X1, X2)
     end
     b = y - w.'*x;
 end
+
+% Function converts a position on a plot to normalized units on the axis
+function [x,y] = toNormalizedUnits(x,y)
+
+    % Determine plot limits
+    xLim = xlim;
+    yLim = ylim;
+
+    % Determine the span of the x and y axis
+    xSpan = xLim(2) - xLim(1);
+    ySpan = yLim(2) - yLim(1);
+
+    % Compute a scale factor for mapping x and y coordinates to pixels
+    ax = gca;
+    xSf = (ax.Position(3)*ax.Parent.Position(3))/xSpan;
+    ySf = (ax.Position(4)*ax.Parent.Position(4))/ySpan;
+
+    % Take the minimum scaling for square axis
+    sf = min([xSf,ySf]);
+
+    % Determine scale factors for mapping x and y cordinates to normalized
+    % units
+    xSf = sf/ax.Parent.Position(3);
+    ySf = sf/ax.Parent.Position(4);
+
+    % Determin normalized position for start of xspan and start of yspan
+    xCenter = ax.Position(1) + ax.Position(3)/2;
+    yCenter = ax.Position(2) + ax.Position(4)/2;
+    xStart = xCenter - xSpan/2*xSf;
+    yStart = yCenter - ySpan/2*ySf;
+
+    % Convert x-y position to normalized units
+    x = (x - xLim(1))*xSf + xStart;
+    y = (y - yLim(1))*ySf + yStart;
+end
+
+% Create a double arrow at x-y positions on the axis
+function doublearrow(x,y)
+    [x,y] = toNormalizedUnits(x,y);
+    annotation('doublearrow',x,y)
+end
+
+
+
+
