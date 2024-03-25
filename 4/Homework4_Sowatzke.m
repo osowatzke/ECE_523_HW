7f4905c9620edb9d744b1755ade552ebe4bc24e8
Owen <osowatzke@gmail.com>
2024-03-25 00:46:06 -0700

    Adding results for question 4

--- 4/Homework4_Sowatzke.m
+++ 4/Homework4_Sowatzke.m
@@ -1,36 +1,38 @@
+% Clear workspace
 clear;
-close all;
+
+% Set random number generator seed for repeatability
 rng('default');
+
+% Load IRIS training data
 C = readcell('IRIS_data.xlsx');
 data = cell2mat(C(:,1:4));
 labels = C(:,5);
 
+% Reformat class labels
 labels = categorical(labels);
+
+% Determine the number of unique classes
 classes = unique(labels);
 
+% Reformat data
 data = data(:,3:4).';
 data = reshape(data,size(data,1),1,1,[]);
 
+% Divide data into training and test sets
 cv = cvpartition(labels,'KFold',5);
 isTraining = cv.training(1);
 isTest = cv.test(1);
 
-% numTrainingSamplesPerClass = 40;
-% isTrainingSample = false(size(data,4),1);
-% for i = 1:length(classes)
-%     classIdx = (1:length(labels)).'.*(labels == classes(i));
-%     classIdx = classIdx(classIdx ~= 0);
-%     classIdx = classIdx(randperm(length(classIdx)));
-%     isTrainingSample(classIdx(1:numTrainingSamplesPerClass)) = true;
-% end
-% isTestSample = ~isTrainingSample;
-
+% Get labels and data for training sets
 trainingLabels = labels(isTraining);
 trainingData = data(:,:,:,isTraining);
 
+% Get labels and data for test sets
 testLabels = labels(isTest);
 testData = data(:,:,:,isTest);
 
+% Construct neural network
 layers = [
     imageInputLayer([size(data,1),1,1]);
     fullyConnectedLayer(5);
@@ -39,6 +41,7 @@ layers = [
     softmaxLayer;
     classificationLayer];
 
+% Setup neural network training options
 opts = trainingOptions('adam',...
     'Shuffle','every-epoch',...
     'LearnRateSchedule','piecewise',...
@@ -46,20 +49,52 @@ opts = trainingOptions('adam',...
     'LearnRateDropFactor',0.5,...
     'L2Regularization',0.001,...
     'MaxEpochs',100,...
-    'MiniBatchSize',4,...
+    'MiniBatchSize',8,...
     'ExecutionEnvironment','auto',...
     'Plots','training-progress',...
     'Verbose',true);
 
+% train network
 net = trainNetwork(trainingData,trainingLabels,layers,opts);
-Z = predict(net,testData);
 
+% Determine how well network performs with training data
+Z = predict(net,trainingData);
 [~,I] = max(Z,[],2);
 predictedLabels = classes(I);
 
+% Create confusion matrix for training data set
+confusionMatrix = zeros(length(classes));
+for i = 1:length(classes)
+    classPredictions = predictedLabels(trainingLabels == classes(i));
+    confusionMatrix(i,:) = sum(classPredictions == classes.');
+end
+disp('Confusion Matrix for Training Dataset:')
+disp(confusionMatrix)
+
+% Compute classification accuracy for training dataset
+% Assume equal prior probabilities
+pCorrect =  diag(confusionMatrix)./sum(confusionMatrix,2);
+pCorrect = mean(pCorrect);
+disp('Classification Accuracy for Training Dataset:')
+disp(pCorrect)
+
+% Determine how well network performs with test data
+Z = predict(net,testData);
+[~,I] = max(Z,[],2);
+predictedLabels = classes(I);
+
+% Create confusion matrix for test data set
 confusionMatrix = zeros(length(classes));
 for i = 1:length(classes)
     classPredictions = predictedLabels(testLabels == classes(i));
     confusionMatrix(i,:) = sum(classPredictions == classes.');
 end
+disp('Confusion Matrix for Test Dataset:')
 disp(confusionMatrix)
+
+% Compute classification accuracy for test dataset
+% Assume equal prior probabilities
+pCorrect =  diag(confusionMatrix)./sum(confusionMatrix,2);
+pCorrect = mean(pCorrect);
+disp('Classification Accuracy for Test Dataset:')
+disp(pCorrect)
