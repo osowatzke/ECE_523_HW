7187e887c597ad87049e1ce3e639828e9b66e1c5
Owen <osowatzke@gmail.com>
2024-04-22 00:17:32 -0700

    Saving work on homework 6

--- 6/Homework6_Sowatzke.m
+++ 6/Homework6_Sowatzke.m
@@ -1,9 +1,12 @@
-%% Cleanup workspace
+%% Setup workspace
 clear;
 close all;
 
+% Flag specifies whether to do training or not
+doTraining = false;
+
 %% Load training data
-[trainingInput, ~] = loadMNISTData('train');
+[trainingInput, trainingLabels] = loadMNISTData('train');
 
 % permute training data
 trainingInput = permute(trainingInput, [1 2 4 3]);
@@ -42,9 +45,11 @@ lgraph = connectLayers(lgraph,'mpool1/indices','unpool1/indices');
 lgraph = connectLayers(lgraph,'mpool1/size','unpool1/size');
 
 %% Train network
+
 % Fix random number generator seed for repeatability
 rng('default');
 
+% Define network training options
 trainingOptions = trainingOptions('adam', ...
     'Shuffle', 'every-epoch',...
     'InitialLearnRate', 1e-3, ...
@@ -58,26 +63,135 @@ trainingOptions = trainingOptions('adam', ...
     'Plots','training-progress',...
     'Verbose', true);
 
-% load('net.mat')
-% 
-% lgraph = layerGraph(net.Layers);
-% 
-% % Connect max pooling and unpooling layers
-% lgraph = connectLayers(lgraph,'mpool2/indices','unpool2/indices');
-% lgraph = connectLayers(lgraph,'mpool2/size','unpool2/size'); 
-% 
-% lgraph = connectLayers(lgraph,'mpool1/indices','unpool1/indices');
-% lgraph = connectLayers(lgraph,'mpool1/size','unpool1/size');
+if doTraining
+    net = trainNetwork(trainingInput,trainingOutput,lgraph,trainingOptions);
+else
+    load('MNISTNet.mat','net');
+end
 
-net = trainNetwork(trainingInput,trainingOutput,lgraph,trainingOptions);
+% Run the training data trough the network
+measTrainingOutput = predict(net, trainingInput);
+
+% Get MSE for training data
+N = numel(trainingOutput);
+err = sum((trainingOutput - measTrainingOutput).^2,'all')/N;
+
+% Display the MSE for the training data set
+disp("MSE for Training Dataset:")
+disp(err)
 
 %% Load test data
-[testInput, ~] = loadMNISTData('test');
+[testInput, testLabels] = loadMNISTData('test');
 
 % permute training data
 testInput = permute(testInput, [1 2 4 3]);
 
-% Get network output
+% Get reference network output
 testOutput = flip(flip(testInput,1),2);
 
-measuredOutput = predict(net,testInput);
+%% Run network on test data
+measTestOutput = predict(net,testInput);
+
+%% Display Images from Training and Test Data Set
+
+% Display sample results from training set
+f = figure(1);
+f.Position = [100, 200, 1050, 400];
+clf;
+t = tiledlayout(2,5,'TileSpacing','compact');
+for i = 1:10
+    nexttile;
+    idx = find(trainingLabels == (i - 1), 1);
+    img = measTrainingOutput(:,:,:,idx);
+    imshow(img);
+end
+title(t, 'Results for Training Dataset')
+
+% Display sample results from test set
+f = figure(2);
+f.Position = [100, 200, 1050, 400];
+clf;
+t = tiledlayout(2,5,'TileSpacing','compact');
+for i = 1:10
+    nexttile;
+    idx = find(testLabels == (i - 1), 1);
+    img = measTestOutput(:,:,:,idx);
+    imshow(img);
+end
+title(t, 'Results for Test Dataset')
+
+%% Compute MSE for Each Digit
+
+% Compute MSE for training data set
+mseTraining = zeros(10,1);
+for i = 1:length(mseTraining)
+    sampleSelect = (trainingLabels == (i - 1));
+    measData = measTrainingOutput(:,:,:,sampleSelect);
+    refData = trainingOutput(:,:,:,sampleSelect);
+    N = numel(measData);
+    mseTraining(i) = sum((measData - refData).^2,'all')/N;
+end
+
+% Output MSE for training data set
+disp('MSE for Training Dataset:')
+fprintf('\n\tDigit%10s\n', 'MSE')
+for i = 1:length(mseTraining)
+    fprintf('\t%5d%10.2f\n',i-1, mseTraining(i));
+end
+fprintf('\n');
+
+% Compute MSE for test data set
+mseTest = zeros(10,1);
+for i = 1:length(mseTest)
+    sampleSelect = (testLabels == (i - 1));
+    measData = measTestOutput(:,:,:,sampleSelect);
+    refData = testOutput(:,:,:,sampleSelect);
+    N = numel(measData);
+    mseTest(i) = sum((measData - refData).^2,'all')/N;
+end
+
+% Output MSE for training data set
+disp('MSE for Test Dataset:')
+fprintf('\n\tDigit%10s\n', 'MSE')
+for i = 1:length(mseTest)
+    fprintf('\t%5d%10.2f\n',i-1, mseTest(i));
+end
+fprintf('\n');
+
+%% Create Histograms of Activation Maps
+
+% Get the activation maps of the bottleneck layer
+activationMaps = activations(net,trainingInput,'conv_3');
+
+% Flatten the activation maps
+activationMaps = reshape(activationMaps,400,[]);
+
+% Create distribution graphs for the first 5 features
+figure(3)
+clf;
+t = tiledlayout(2,3);
+for i = 1:5
+    nexttile;
+    histogram(activationMaps(i,:));
+end
+title(t,'Histograms for Activation Map Features')
+
+%% Approximate Distributions of First Five Features
+
+% Compute the mean and variance of the first five features
+mu = mean(activationMaps(1:5,:), 2);
+sigma = var(activationMaps(1:5,:), 0, 2);
+
+% Display Mean and Variance
+fprintf('%10s%10s\n','Mean','Variance')
+for i = 1:length(mu)
+    fprintf('%10.2f%10.2f\n', mu(i), sigma(i));
+end
+fprintf('\n');
+
+% Compute the covariance matrix
+C = cov(activationMaps(1:5,:).');
+
+% Display the covariance matrix
+disp('Covariance Matrix:')
+disp(C)
